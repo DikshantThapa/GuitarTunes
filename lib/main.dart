522458e6dc842ef10d91b13024f7576e3b212928
Dikshant Thapa <dikshantthapa3@gmail.com>
2023-02-12 15:25:38 +0545

    Added splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      home: Splashscreen(),
 
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
