5252d67380a386996953fa3883dc6f322359da66
Dikshant Thapa <dikshantthapa3@gmail.com>
2023-02-14 12:57:28 +0545

    Icon Added Into application and setting page half completed

--- lib/pages/SettingPage.dart
+++ lib/pages/SettingPage.dart
@@ -3,11 +3,292 @@ import 'package:flutter/material.dart';
 class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
-      child: SafeArea(
-        child: Center(
-          child: Text('Settings'),
+    return Scaffold(
+      backgroundColor: Colors.transparent
+      ,
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: false,
+        backgroundColor: Colors.transparent,
+        title: Text("Settings",style: TextStyle(
+           fontWeight: FontWeight.bold,
+            fontSize: 36,
+        ),
+        
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+        
+          children: [
+            Row(
+              children: [
+                Container(
+                  height: 57,
+                  padding: EdgeInsets.only(left: 18),
+                  width: 200,
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "General",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                   // border: Border.all(color: Colors.white)
+                  ),
+                ),
+                SizedBox(
+                  width: 200,
+                )
+              ],
+            ),
+
+            Container(
+              margin:EdgeInsets.only(top:5,bottom:5),
+              padding: EdgeInsetsDirectional.symmetric(vertical: 2,horizontal: 8),
+              height: 60,
+              alignment:Alignment.center,
+              width: 600,
+
+              decoration: BoxDecoration(
+                //ßborder: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(30)
+              ),
+            ),
+
+            Container(
+              margin:EdgeInsets.only(top:5,bottom:5),
+              padding: EdgeInsetsDirectional.symmetric(vertical: 2,horizontal: 8),
+              height: 60,
+              alignment:Alignment.center,
+              width: 600,
+
+              decoration: BoxDecoration(
+               //ß border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(30)
+              ),
+            ),
+
+            Container(
+              margin:EdgeInsets.only(top:5,bottom:5),
+              padding: EdgeInsetsDirectional.symmetric(vertical: 2,horizontal: 8),
+              height: 60,
+              alignment:Alignment.center,
+              width: 600,
+
+              decoration: BoxDecoration(
+              //ß  border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(30)
+              ),
+            ),
+
+            Container(
+              margin:EdgeInsets.only(top:5,bottom:5),
+              padding: EdgeInsetsDirectional.symmetric(vertical: 2,horizontal: 8),
+              height: 60,
+              alignment:Alignment.center,
+              width: 600,
+
+              decoration: BoxDecoration(
+              //ß  border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(30)
+              ),
+            ),
+            Container(
+              margin:EdgeInsets.only(top:5,bottom:5),
+              padding: EdgeInsetsDirectional.symmetric(vertical: 2,horizontal: 8),
+              height: 60,
+              alignment:Alignment.center,
+              width: 600,
+
+              decoration: BoxDecoration(
+              //ß  border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(30)
+              ),
+            ),
+
+            Container(
+              margin:EdgeInsets.only(top:5,bottom:5),
+              padding: EdgeInsetsDirectional.symmetric(vertical: 2,horizontal: 8),
+              height: 60,
+              alignment:Alignment.center,
+              width: 600,
+
+              decoration: BoxDecoration(
+              //ß  border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(30)
+              ),
+            ),
+
+            Container(
+              margin:EdgeInsets.only(top:5,bottom:5),
+              padding: EdgeInsetsDirectional.symmetric(vertical: 2,horizontal: 8),
+              height: 60,
+              alignment:Alignment.center,
+              width: 600,
+
+              decoration: BoxDecoration(
+             //ß   border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(30)
+              ),
+            ),
+
+            SizedBox(
+              height: 19,
+            ),
+
+              Row(
+                children: [
+                  Container(
+                      height: 57,
+                      padding: EdgeInsets.only(left: 18),
+                      width: 200,
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Chords",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                      //  border: Border.all(color: Colors.white)
+                      ),
+                    ),
+
+                    SizedBox(
+                      width: 200,
+                    )
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+
+            Container(
+              margin:EdgeInsets.only(top:5,bottom:5),
+              padding: EdgeInsetsDirectional.symmetric(vertical: 2,horizontal: 8),
+              height: 60,
+              alignment:Alignment.center,
+              width: 600,
+
+              decoration: BoxDecoration(
+              //ß  border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(30)
+              ),
+            ),
+
+            Container(
+              margin:EdgeInsets.only(top:5,bottom:5),
+              padding: EdgeInsetsDirectional.symmetric(vertical: 2,horizontal: 8),
+              height: 60,
+              alignment:Alignment.center,
+              width: 600,
+
+              decoration: BoxDecoration(
+              //ß  border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(30)
+              ),
+            ),
+
+            //Subscription
+            SizedBox(height: 20,),
+
+            Row(
+                children: [
+                  Container(
+                      height: 57,
+                      padding: EdgeInsets.only(left: 18),
+                      width: 200,
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Subscription",
+                        style: TextStyle(
+                //ß          color: Colors.white,
+                          fontSize: 30
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                      //  border: Border.all(color: Colors.white)
+                      ),
+                    ),
+
+                    SizedBox(
+                      width: 200,
+                    )
+                ],
+              ),
+              SizedBox(height: 20,),
+
+            Container(
+              margin:EdgeInsets.only(top:5,bottom:5),
+              padding: EdgeInsetsDirectional.symmetric(vertical: 2,horizontal: 8),
+              height: 60,
+              alignment:Alignment.center,
+              width: 600,
+
+              decoration: BoxDecoration(
+              //ß  border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(30)
+              ),
+            ),
+
+            Container(
+              margin:EdgeInsets.only(top:5,bottom:5),
+              padding: EdgeInsetsDirectional.symmetric(vertical: 2,horizontal: 8),
+              height: 60,
+              alignment:Alignment.center,
+              width: 600,
+
+              decoration: BoxDecoration(
+              //ß  border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(30)
+              ),
+            ),
+
+            Container(
+              margin:EdgeInsets.only(top:5,bottom:5),
+              padding: EdgeInsetsDirectional.symmetric(vertical: 2,horizontal: 8),
+              height: 60,
+              alignment:Alignment.center,
+              width: 600,
+
+              decoration: BoxDecoration(
+              //ß  border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(30)
+              ),
+            ),
+
+            Container(
+              margin:EdgeInsets.only(top:5,bottom:5),
+              padding: EdgeInsetsDirectional.symmetric(vertical: 2,horizontal: 8),
+              height: 60,
+              alignment:Alignment.center,
+              width: 600,
+
+              decoration: BoxDecoration(
+              //ß  border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(30)
+              ),
+            ),
+
+            Container(
+              margin:EdgeInsets.only(top:5,bottom:5),
+              padding: EdgeInsetsDirectional.symmetric(vertical: 2,horizontal: 8),
+              height: 60,
+              alignment:Alignment.center,
+              width: 600,
+
+              decoration: BoxDecoration(
+              //ß  border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(30)
+              ),
+            ),
+
+           
+          ],
         ),
       ),
     );
